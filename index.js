1e3e63bc80e9964cfc3377b8e94e674cc85222c7
ThoriqFatih <129358222+ThoriqFatih@users.noreply.github.com>
2023-10-16 08:54:08 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+fetch('http://apilayer.net/api/live?access_key=d684f70cc5a61f172dc837c590595856&pairs=EURUSD,EURGBP,GBPUSD,USDJPY,AUDUSD,USDCHF,NZDUSD,USDCAD,USDZAR')
+    .then(res => res.json())
+    .then(json => {
+        
+    }
